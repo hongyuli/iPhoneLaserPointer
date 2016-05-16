4f7042c968afe568e8f5e675abafdbb64a3de404
hongyuli <lihongyu728@gmail.com>
2016-05-16 12:27:10 -0400

    add delegate on ViewController

---
+++ GyroAccel/IMUDataViewControllerDelegate.swift
@@ -0,0 +1,9 @@
+//
+//  IMUDataViewControllerDelegate.swift
+//  GyroAccel
+//
+//  Created by LiHongyu on 5/15/16.
+//  Copyright © 2016 Andrew Genualdi. All rights reserved.
+//
+
+import Foundation
